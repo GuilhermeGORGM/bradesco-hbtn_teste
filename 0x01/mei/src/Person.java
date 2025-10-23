7311aa97e5859abd614cd858115ee68fd73587e2
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-22 21:04:02 -0300

    junit

--- 0x01/mei/src/Person.java
+++ 0x01/mei/src/Person.java
@@ -5,113 +5,113 @@ import java.util.Date;
 
 public class Person {
 
-    private String name;
-    private String surname;
-    private Date birthDate;
-    private boolean anotherCompanyOwner;
-    private boolean pensioner;
-    private boolean publicServer;
-    private float salary;
+	private String name;
+	private String surname;
+	private Date birthDate;
+	private boolean anotherCompanyOwner;
+	private boolean pensioner;
+	private boolean publicServer;
+	private float salary;
 
-    public Person(String name, String surname, Date birthDate,
-                  boolean anotherCompanyOwner, boolean pensioner, boolean publicServer, float salary) {
-        this.name = name;
-        this.surname = surname;
-        this.birthDate = birthDate;
-        this.anotherCompanyOwner = anotherCompanyOwner;
-        this.pensioner = pensioner;
-        this.publicServer = publicServer;
-        this.salary = salary;
-    }
+	public Person(String name, String surname, Date birthDate, boolean anotherCompanyOwner, boolean pensioner,
+			boolean publicServer, float salary) {
+		this.name = name;
+		this.surname = surname;
+		this.birthDate = birthDate;
+		this.anotherCompanyOwner = anotherCompanyOwner;
+		this.pensioner = pensioner;
+		this.publicServer = publicServer;
+		this.salary = salary;
+	}
 
-    // Getters e Setters
-    public String getName() {
-        return name;
-    }
+	// Getters e Setters
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public Date getBirthDate() {
-        return birthDate;
-    }
+	public Date getBirthDate() {
+		return birthDate;
+	}
 
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
-    public boolean isAnotherCompanyOwner() {
-        return anotherCompanyOwner;
-    }
+	public boolean isAnotherCompanyOwner() {
+		return anotherCompanyOwner;
+	}
 
-    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
-        this.anotherCompanyOwner = anotherCompanyOwner;
-    }
+	public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+		this.anotherCompanyOwner = anotherCompanyOwner;
+	}
 
-    public boolean isPensioner() {
-        return pensioner;
-    }
+	public boolean isPensioner() {
+		return pensioner;
+	}
 
-    public void setPensioner(boolean pensioner) {
-        this.pensioner = pensioner;
-    }
+	public void setPensioner(boolean pensioner) {
+		this.pensioner = pensioner;
+	}
 
-    public boolean isPublicServer() {
-        return publicServer;
-    }
+	public boolean isPublicServer() {
+		return publicServer;
+	}
 
-    public void setPublicServer(boolean publicServer) {
-        this.publicServer = publicServer;
-    }
+	public void setPublicServer(boolean publicServer) {
+		this.publicServer = publicServer;
+	}
 
-    public float getSalary() {
-        return salary;
-    }
+	public float getSalary() {
+		return salary;
+	}
 
-    public void setSalary(float salary) {
-        this.salary = salary;
-    }
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 
-    // Retorna nome completo
-    public String fullName() {
-        return this.name + " " + this.surname;
-    }
+	// Retorna nome completo
+	public String fullName() {
+		return this.name + " " + this.surname;
+	}
 
-    // Calcula salário anual
-    public float calculateYearlySalary() {
-        return this.salary * 12;
-    }
+	// Calcula salário anual
+	public float calculateYearlySalary() {
+		return this.salary * 12;
+	}
 
-    // Calcula idade com base na data de nascimento
-    private int getAge() {
-        Calendar today = Calendar.getInstance();
-        Calendar birth = Calendar.getInstance();
-        birth.setTime(this.birthDate);
-        int age = today.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+	// Calcula idade com base na data de nascimento
+	private int getAge() {
+		Calendar today = Calendar.getInstance();
+		Calendar birth = Calendar.getInstance();
+		birth.setTime(this.birthDate);
+		int age = today.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
 
-        if (today.get(Calendar.DAY_OF_YEAR) < birth.get(Calendar.DAY_OF_YEAR)) {
-            age--;
-        }
-        return age;
-    }
+		if (today.get(Calendar.DAY_OF_YEAR) < birth.get(Calendar.DAY_OF_YEAR)) {
+			age--;
+		}
+		return age;
+	}
 
-    // Verifica se a pessoa pode ser MEI
-    public boolean isMEI() {
-        boolean incomeOk = calculateYearlySalary() < 130000;
-        boolean ageOk = getAge() >= 18;
-        boolean ownershipOk = !anotherCompanyOwner;
-        boolean pensionOk = !pensioner;
-        boolean serverOk = !publicServer;
+	// Verifica se a pessoa pode ser MEI
+	public boolean isMEI() {
+		boolean incomeOk = calculateYearlySalary() < 130000;
+		boolean ageOk = getAge() >= 18;
+		boolean ownershipOk = !anotherCompanyOwner;
+		boolean pensionOk = !pensioner;
+		boolean serverOk = !publicServer;
 
-        return incomeOk && ageOk && ownershipOk && pensionOk && serverOk;
-    }
+		return incomeOk && ageOk && ownershipOk && pensionOk && serverOk;
+	}
 }
